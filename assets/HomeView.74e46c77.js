f1ca7754219f2e33f9abb43bddbc7499866e1559
yunsiou6628 <101792849+yunsiou6628@users.noreply.github.com>
2022-07-18 01:42:24 +0000

    Deploying to gh-pages from @ yunsiou6628/vue-project@5947972bd25a71108507393d8895cc357d34aa6c 🚀

---
+++ assets/HomeView.74e46c77.js
@@ -0,0 +1,1 @@
+import{o as a,a6 as o,b as e,w as t,V as s,d as c,g as r,b6 as n}from"./index.01c9f2a1.js";const d={id:"admin-home"},_=r("h1",{class:"text-center"},"\u8ACB\u9078\u64C7\u7BA1\u7406\u9805\u76EE",-1);function l(f,m){return a(),o("div",d,[e(s,null,{default:t(()=>[e(c,{cols:"12"},{default:t(()=>[_]),_:1})]),_:1})])}const i={};var u=n(i,[["render",l]]);export{u as default};
